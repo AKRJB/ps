4ebaf524111ce7218d7b5789b679e1df69140e3b
Alakh Kaler <alakh@Alakhs-MacBook-Air.local>
2024-03-11 23:22:55 +0530

    bug fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,33 +4,37 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import { NavLink } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 
 function CustomNavbar() {
   return (
     <Navbar expand="lg" className="navbar navbar-dark bg-dark">
       <Container fluid>
-        <Navbar.Brand to ="/">Pioneer Scientific Industries</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Pioneer Scientific Industries</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <NavLink className="text-white" to ='/'>Home</NavLink>
+          <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: '100px' }} navbarScroll>
+            <LinkContainer to="/">
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
             <NavDropdown title="Products" id="navbarScrollingDropdown">
-                <NavDropdown.Item to ="#all-products">All Products</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item to ="#charts">Charts</NavDropdown.Item>
-                <NavDropdown.Item to ="#microscopic-slides">Microscopic Slides</NavDropdown.Item>
-                <NavDropdown.Item to ="#medical-charts">Medical Charts-Models</NavDropdown.Item>
-                <NavDropdown.Item to ="#technical-institutes">Technical Institutes</NavDropdown.Item>
-                <NavDropdown.Item to ="#nursing">Nursing</NavDropdown.Item>
+              <LinkContainer to="#all-products">
+                <NavDropdown.Item>All Products</NavDropdown.Item>
+              </LinkContainer>
+              <NavDropdown.Divider />
+              <LinkContainer to="#charts">
+                <NavDropdown.Item>Charts</NavDropdown.Item>
+              </LinkContainer>
+              {/* Add LinkContainer to other NavDropdown items as needed */}
             </NavDropdown>
-            <NavLink className="text-white" to ='/contact'>Contact us</NavLink>
-            <NavLink className="text-white" to ="/about">About us</NavLink>
-
+            <LinkContainer to="/contact">
+              <Nav.Link>Contact us</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about">
+              <Nav.Link>About us</Nav.Link>
+            </LinkContainer>
           </Nav>
           <Form className="d-flex">
             <Form.Control
